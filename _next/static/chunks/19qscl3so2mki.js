6241ca6acac793318a1b38f230d84c50e4f027a2
Maycon-MB <Maycon-MB@users.noreply.github.com>
2026-09-12 21:55:33 +0000

    publica 7234f65161c65032a5e0e047ade21c0501ad5d0e 7234f65161c65032a5e0e047ade21c0501ad5d0e

---
+++ _next/static/chunks/19qscl3so2mki.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,2338,t=>{t.v(s=>Promise.all(["static/chunks/0ogt72b9wrtez.js"].map(s=>t.l(s))).then(()=>s(4594)))},11647,t=>{t.v(s=>Promise.all(["static/chunks/0bsudrys1-2g6.js"].map(s=>t.l(s))).then(()=>s(2561)))}]);
